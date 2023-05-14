836794e8432ef2e422eee7645aabfd75738e16b1
yunkhngn <rogkhoa.mail@gmail.com>
2023-05-15 00:26:07 +0700

    Updating database path

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const res = require('express/lib/response');
 const app = express();
 const fs =require('fs')
+const path = require('path');
 
 const databasePath = path.join(__dirname, 'database.json')
 
