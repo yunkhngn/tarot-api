5b935ec32c6873607f00828badc140bebd00c74b
yunkhngn <rogkhoa.mail@gmail.com>
2023-05-15 00:35:04 +0700

    Update header

--- api/index.js
+++ api/index.js
@@ -13,15 +13,18 @@ console.log(databasePath)
 
 app.get('/api', (req, res) => {
   const response = { status: "Successfully deploying API" };
+  res.set('title', 'Tarot API')
   res.send(response);
 });
 
 app.get('/api/author', (req, res) => {
+  res.set('title', 'Author')
   res.send({ author: "@yun.khngn", date_created: "15/5/2023" });
 });
 
 // Trả về tất cả các lá bài
 app.get('/api/cards', (req, res) => {
+  res.set('title', 'Cards database')
   res.json(database.cards)
 })
 
@@ -32,12 +35,14 @@ app.get('/api/cards/:slug', (req, res) => {
   if (!card) {
     return res.status(404).json({ error: 'Card not found' })
   }
+  res.set('title', card.name)
   res.json(card)
 })
 
 // Random 3 lá bài
 app.get('/api/random', (req, res) => {
   const cards = database.cards.sort(() => 0.5 - Math.random()).slice(0, 3)
+  res.set('title', 'Random 3 cards')
   res.json(cards)
 })
 
@@ -52,6 +57,7 @@ app.get('/api/cards/:slug/:field/', (req, res) => {
   }
 
   if (card[field]) {
+    res.set('title', `${card.name} - ${field}`)
     res.json({ 
       [field]: card[field] })
   } else {
